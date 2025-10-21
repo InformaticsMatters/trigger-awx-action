35b52cb751efcc31fdffc6c4d815965d3ee44001
Alan B. Christie <29806285+alanbchristie@users.noreply.github.com>
2025-10-21 21:47:00 +0200

    Document recent change

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,4 +22,6 @@ TEMPLATE_VAR_VALUE=$6
 EXTRA_VARS={\"${TEMPLATE_VAR}\":\"${TEMPLATE_VAR_VALUE}\"}
 echo "Launching Job Template ${TEMPLATE} and monitoring..."
 echo "EXTRA_VARS=${EXTRA_VARS}"
+# We redirect stdout to /dev/null
+# otherwise sensitive AWX variables may be exposed to the GitHub Action Log
 awx job_templates launch --monitor -e ${EXTRA_VARS} "${TEMPLATE}" > /dev/null
